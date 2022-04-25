cc920f065fe636c6392b039e9f12b1e080bb3d0f
bonono63 <bonoria63@gmail.com>
2022-04-24 23:41:56 -0400

    Added ContraptionDimensionHandler, handles all the instances of Contraption needed to make the entities function, hopefully.

---
+++ src/main/java/net/mrbonono63/scarlet/server/ContraptionDimensionHandler.java
@@ -0,0 +1,45 @@
+package net.mrbonono63.scarlet.server;
+
+import net.mrbonono63.scarlet.server.contraption.Contraption;
+
+import java.util.*;
+
+public class ContraptionDimensionHandler {
+
+    public  List<Contraption> contraptionList = new ArrayList<>();
+
+    int previousListSize = 0;
+    int ListSize = 0;
+
+    public ContraptionDimensionHandler() {
+
+    }
+
+    public  void addContraption(Contraption contraption)
+    {
+        contraptionList.add(contraption);
+    }
+
+    public  void removeContraption(int i)
+    {
+        contraptionList.remove(i);
+    }
+
+    public void tick()
+    {
+        this.previousListSize = this.ListSize;
+        this.ListSize = contraptionList.size();
+    }
+
+    //assuming that the new contraption is added on the end of the contraptionList this will get the contraption's
+    //position in the list and allow it to remove itself in the future by adding
+    public int getListSize()
+    {
+        return this.ListSize;
+    }
+
+    public Contraption getContraption(int listIndex)
+    {
+        return this.contraptionList.get(listIndex);
+    }
+}
