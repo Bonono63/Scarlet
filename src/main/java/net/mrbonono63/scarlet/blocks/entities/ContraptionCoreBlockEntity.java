8f7aee3cc79441dbc512376e17e4aa72cd376686
SwigS <23MaufortR@mydacc.org>
2022-04-29 09:09:10 -0400

    Create ContraptionCoreBlockEntity.java

---
+++ src/main/java/net/mrbonono63/scarlet/blocks/entities/ContraptionCoreBlockEntity.java
@@ -0,0 +1,13 @@
+package net.mrbonono63.scarlet.blocks.entities;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.util.math.BlockPos;
+
+public class ContraptionCoreBlockEntity extends BlockEntity {
+
+    public ContraptionCoreBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+}
