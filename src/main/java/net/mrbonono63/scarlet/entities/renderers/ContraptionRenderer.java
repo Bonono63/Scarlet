f0a98203066d7dd8ec67ddf8c24ba84aa0e1c9eb
bonono63 <bonoria63@gmail.com>
2022-04-30 15:49:50 -0400

    Added a place holder render

--- src/main/java/net/mrbonono63/scarlet/entities/renderers/ContraptionRenderer.java
+++ src/main/java/net/mrbonono63/scarlet/entities/renderers/ContraptionRenderer.java
@@ -1,8 +1,14 @@
 package net.mrbonono63.scarlet.entities.renderers;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Frustum;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.BlockRenderManager;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import net.mrbonono63.scarlet.entities.ContraptionEntity;
 
@@ -18,7 +24,14 @@ public class ContraptionRenderer extends EntityRenderer<ContraptionEntity> {
     }
 
     @Override
-    public boolean shouldRender(ContraptionEntity entity, Frustum frustum, double x, double y, double z) {
-        return true;
+    public void render(ContraptionEntity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+        matrices.push();
+
+        BlockRenderManager blockRenderManager = MinecraftClient.getInstance().getBlockRenderManager();
+
+        blockRenderManager.renderBlockAsEntity(Blocks.OAK_PLANKS.getDefaultState(), matrices, vertexConsumers, light, 1);
+
+        matrices.pop();
+        super.render(entity, yaw, tickDelta, matrices, vertexConsumers, light);
     }
 }
