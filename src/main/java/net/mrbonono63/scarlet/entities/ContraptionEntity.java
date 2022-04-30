1fd9c86fb7ef3af8e4b3f631be4711c5839a4979
bonono63 <bonoria63@gmail.com>
2022-04-30 15:49:24 -0400

    Updated for use with BlockBoxes instead of Contraptions

--- src/main/java/net/mrbonono63/scarlet/entities/ContraptionEntity.java
+++ src/main/java/net/mrbonono63/scarlet/entities/ContraptionEntity.java
@@ -1,7 +1,6 @@
 package net.mrbonono63.scarlet.entities;
 
 
-import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LightningEntity;
@@ -14,27 +13,22 @@ import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
+import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.EulerAngle;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 import net.mrbonono63.scarlet.Main;
 import net.mrbonono63.scarlet.blocks.SBlocks;
-import net.mrbonono63.scarlet.server.Contraption;
-import net.mrbonono63.scarlet.util.NBTReadBlockPos;
+import net.mrbonono63.scarlet.util.NBTUtil;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.Arrays;
 
 public class ContraptionEntity extends Entity {
 
     //Local instance of the contraption entity that is then added to the Contraption List
-    Contraption contraption = new Contraption(new Identifier("", ""), new BlockPos(0,0,0), new BlockPos(0,0,0), new BlockPos(0,0,0), new BlockPos(0,0,0));
+    BlockBox contraption = BlockBox.create(Vec3i.ZERO, Vec3i.ZERO);
     // Is used to delete the contraption from the List
     private int contraptionListIndex = 0;
     private World world;
@@ -87,11 +81,11 @@ public class ContraptionEntity extends Entity {
     }
 
     //Used when making the entity form a block
-    public ContraptionEntity(EntityType<?> type, World world, Contraption contraption)
+    public ContraptionEntity(EntityType<?> type, World world, BlockBox box)
     {
         super(type, world);
         //this.intersectionChecked = true;
-        this.contraption = contraption;
+        this.contraption = box;
         this.world = world;
     }
 
@@ -104,21 +98,24 @@ public class ContraptionEntity extends Entity {
 
     //Assemble and Disassemble
 
-    public void Assemble(Contraption contraption)
+    public void Assemble(BlockBox contraption)
     {
         Main.contraptionDimensionHandler.addContraption(contraption);
-        // this needs to be called immediately after the contraption is added to the end of the list, otherwise it will
-        // delete the wrong contraption from the contraptionList in Main.
+        //this needs to be called immediately after the contraption is added to the end of the list, otherwise it will
+        //delete the wrong contraption from the contraptionList in Main.
         this.contraptionListIndex = Main.contraptionDimensionHandler.getListSize();
         //decide how many maximum passengers there are and where the mount points are located in the entity
         //Also needs to be done for block entities and regular blocks that can be interacted with, ( really just if
-        // possible every interaction including breaking blocks needs to be sent to the contraption inside the
-        // dimension to allow for literally all functionality from vanilla and mods being the best solution)
+        //possible every interaction including breaking blocks needs to be sent to the contraption inside the
+        //dimension to allow for literally all functionality from vanilla and mods being the best solution)
     }
 
     public void Disassemble()
     {
         Main.contraptionDimensionHandler.removeContraption(this.contraptionListIndex);
+
+        //Add a function to add ALL the blocks back to the world along with rotating them
+        //see CloneCommand class
     }
 
     /*
@@ -132,10 +129,12 @@ public class ContraptionEntity extends Entity {
 
     @Override
     protected void readCustomDataFromNbt(NbtCompound nbt) {
-        this.rotation = new EulerAngle(nbt.getFloat("yaw"), nbt.getFloat("pitch"), nbt.getFloat("roll"));
+        this.rotation = new EulerAngle(nbt.getFloat("yaw"),
+                nbt.getFloat("pitch"),
+                nbt.getFloat("roll"));
 
         this.contraption = readContraptionNBTData(nbt);
-        Main.LOGGER.info(this.contraption.getOriginDimensionID().toString() + " " + this.contraption.getOriginCorner1() + " " + this.contraption.getOriginCorner2() + " " + this.contraption.getContraptionCorner1() + " " + this.contraption.getContraptionCorner2());
+        Main.LOGGER.info(String.valueOf(this.contraption));
     }
 
     @Override
@@ -150,23 +149,12 @@ public class ContraptionEntity extends Entity {
 
     public void writeContraptionData(NbtCompound nbt)
     {
-        //write the namespace of the dimension
-        nbt.putString("namespace", this.contraption.originDimensionID.getNamespace());
-        nbt.putString("dim_id", this.contraption.originDimensionID.getPath());
-        NBTReadBlockPos.writeBlockPos("originBlockPos", this.contraption.getOriginCorner1(), nbt);
-        NBTReadBlockPos.writeBlockPos("originBlockPos_", this.contraption.getOriginCorner2(), nbt);
-        NBTReadBlockPos.writeBlockPos("contraptionBlockPos", this.contraption.getContraptionCorner1(), nbt);
-        NBTReadBlockPos.writeBlockPos("contraptionBlockPos_", this.contraption.getContraptionCorner2(), nbt);
+        NBTUtil.writeBlockBox("origin", contraption, nbt);
     }
 
-    public Contraption readContraptionNBTData(NbtCompound nbt)
+    public BlockBox readContraptionNBTData(NbtCompound nbt)
     {
-        BlockPos originBlockPos = NBTReadBlockPos.readBlockPos("originBlockPos",nbt) ;
-        BlockPos originBlockPos_ = NBTReadBlockPos.readBlockPos("originBlockPos_",nbt) ;
-        BlockPos contraptionBlockPos = NBTReadBlockPos.readBlockPos("contraptionBlockPos",nbt) ;
-        BlockPos contraptionBlockPos_ = NBTReadBlockPos.readBlockPos("contraptionBlockPos_",nbt) ;
-
-        return new Contraption(new Identifier (nbt.getString("namespace"), nbt.getString("dim_id")), originBlockPos, originBlockPos_, contraptionBlockPos, contraptionBlockPos_);
+        return NBTUtil.readBlockBox("origin", nbt);
     }
 
     //Uses what everything else uses I don't know what else I would use anyway.
@@ -179,16 +167,8 @@ public class ContraptionEntity extends Entity {
     //Essentially the main entity loop, happens every tick
     @Override
     public void baseTick() {
-        this.checkBlockCollision();
-        //this.setBoundingBox(new Box(0,0,0, 1,1,1));
 
-        if (contraption == null)
-        {
-            Main.LOGGER.info(getEntityName()+" is missing contraption data");
-        }
 
-        RegistryEntry<DimensionType> world_registry = world.method_40134();
-        world_registry.getType();
     }
 
     @Override
