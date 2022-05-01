c1842967832ff623c4af2fc40167dde1c174c246
bonono63 <bonoria63@gmail.com>
2022-04-30 22:39:38 -0400

    Added the contraption Dimension ServerWorld

--- src/main/java/net/mrbonono63/scarlet/server/ContraptionDimensionHandler.java
+++ src/main/java/net/mrbonono63/scarlet/server/ContraptionDimensionHandler.java
@@ -1,20 +1,35 @@
 package net.mrbonono63.scarlet.server;
 
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockBox;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Vec3i;
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
 import net.minecraft.world.chunk.ChunkManager;
+import net.mrbonono63.scarlet.Main;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContraptionDimensionHandler {
 
     //List of the contraptions the server is keeping track of
-    public  List<BlockBox> originList = new ArrayList<>();
-    public  List<BlockBox> denstinationList = new ArrayList<>();
+    public List<BlockBox> originList = new ArrayList<>();
+    public List<BlockBox> denstinationList = new ArrayList<>();
 
     int previousListSize = 0;
     int ListSize = 0;
+
+    //maximum entity length
+    public static final int MAXIMUM_BLOCK_LENGTH = 128;
+    //length in chunks of the maximum block length
+    public static final int CHUNK_SIZE = MAXIMUM_BLOCK_LENGTH / 16;
 
     //constructor
     public ContraptionDimensionHandler() {
@@ -38,9 +53,25 @@ public class ContraptionDimensionHandler {
         this.previousListSize = this.ListSize;
         this.ListSize = originList.size();
 
-        if (!serverWorld.isClient())
-        {
-            ChunkManager chunkManager = serverWorld.getChunkManager();
+        if (!serverWorld.isClient()) {
+            /*for (int i = 0; i < originList.size(); i++) {
+
+            }
+             */
+
+            //serverWorld.getChunkManager().setChunkForced(new ChunkPos(0,0), true);
+
+            MinecraftServer server = serverWorld.getServer();
+
+            serverWorld.setChunkForced(0,0,true);
+
+
+            if (server.getWorldRegistryKeys().contains(RegistryKey.of(Registry.WORLD_KEY, new Identifier("scarlet", "contraption"))))
+            {
+                ServerWorld contraptionDimension =  server.getWorld(RegistryKey.of(Registry.WORLD_KEY, new Identifier("scarlet", "contraption")));
+                contraptionDimension.setChunkForced(0,0, true);
+                Main.LOGGER.info(contraptionDimension.getForcedChunks().toString());
+            }
         }
     }
 
@@ -54,14 +85,7 @@ public class ContraptionDimensionHandler {
     {
         return BlockBox.create(new Vec3i(origin.getMinX(), origin.getMinY(), origin.getMinZ()), new Vec3i(origin.getMaxX(), origin.getMaxY(), origin.getMaxZ()));
     }
-/*
-    public static boolean checkConflicts()
-    {
 
-    }
-
-
- */
     //assuming that the new contraption is added on the end of the contraptionList this will get the contraption's
     //position in the list and allow it to remove itself in the future by adding
     public int getListSize()
