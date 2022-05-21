81bef21f2b6b42bf0c18ad67e71c3d8d461c0f50
bonono63 <bonoria63@gmail.com>
2022-05-21 00:02:29 -0400

    Removed reliance on Immersive Portal's Dimension initializer

--- src/main/java/net/mrbonono63/scarlet/Main.java
+++ src/main/java/net/mrbonono63/scarlet/Main.java
@@ -2,26 +2,16 @@ package net.mrbonono63.scarlet;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.structure.StructureSet;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryEntry;
-import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.dimension.DimensionOptions;
-import net.minecraft.world.dimension.DimensionType;
 import net.mrbonono63.scarlet.blocks.SBlocks;
 import net.mrbonono63.scarlet.blocks.entities.SBlockEntity;
+import net.mrbonono63.scarlet.client.gui.SScreenHandlers;
 import net.mrbonono63.scarlet.entities.SEntity;
 import net.mrbonono63.scarlet.items.SItems;
-import net.mrbonono63.scarlet.server.ContraptionChunkGenerator;
+import net.mrbonono63.scarlet.network.SPackets;
 import net.mrbonono63.scarlet.server.ContraptionDimensionHandler;
-import net.mrbonono63.scarlet.server.SScreenHandlers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import qouteall.q_misc_util.LifecycleHack;
-import qouteall.q_misc_util.api.DimensionAPI;
-
 public class Main implements ModInitializer {
 	public static final String MOD_ID = "scarlet";
 	public static final Logger LOGGER = LoggerFactory.getLogger("Scarlet");
@@ -33,6 +23,8 @@ public class Main implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		SPackets.init();
+		LOGGER.info("Packets Registered");
 		SBlocks.init();
 		LOGGER.info("Blocks have been initialized");
 		SItems.init();
@@ -40,39 +32,9 @@ public class Main implements ModInitializer {
 		SEntity.init();
 		LOGGER.info("Entities have been initialized");
 		SBlockEntity.init();
-		LOGGER.info("Block Entities have been intialized");
+		LOGGER.info("Block Entities have been initialized");
 		SScreenHandlers.init();
-
-
-		DimensionAPI.serverDimensionsLoadEvent.register((generatorOptions, registryManager) -> {
-			Registry<DimensionOptions> registry = generatorOptions.getDimensions();
-
-			// get the dimension type
-			RegistryEntry<DimensionType> dimType = registryManager.get(Registry.DIMENSION_TYPE_KEY).getEntry(
-					RegistryKey.of(Registry.DIMENSION_TYPE_KEY, new Identifier(MOD_ID, "contraption"))
-			).orElseThrow(() -> new RuntimeException("Missing dimension type"));
-
-			Identifier dimId = new Identifier(MOD_ID,"contraption");
-
-			// get the biome registry for initializing the biome source
-			Registry<Biome> biomeRegistry = registryManager.get(Registry.BIOME_KEY);
-			Registry<StructureSet> structureRegistry = registryManager.get(Registry.STRUCTURE_SET_KEY);
-
-			// add the dimension
-			DimensionAPI.addDimension(
-					registry, dimId, dimType,
-					new ContraptionChunkGenerator(structureRegistry, biomeRegistry)
-			);
-
-			// Mark the dimension non-persistent so it won't be saved into level.dat
-			//TODO decide whether to make the dimension persistent (this will decide whether it's contents will be saved or not within the level.dat)
-			DimensionAPI.markDimensionNonPersistent(dimId);
-			LOGGER.info("Scarlet Contraption Dimension marked as non persistent");
-			// Supposed to mark scarlet as stable
-			LifecycleHack.markNamespaceStable("scarlet");
-			LOGGER.info("Scarlet marked as safe namespace");
-		});
-
+		LOGGER.info("Screen Handlers have been initialized");
 
 		ServerTickEvents.START_WORLD_TICK.register((serverWorld) -> {
 			//either compare the index size or the whole list to a previous ticks list to decide whether to update the dimension or not
