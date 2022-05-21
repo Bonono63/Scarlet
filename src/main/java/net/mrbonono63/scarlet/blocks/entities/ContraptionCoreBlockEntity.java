aa372cd6ad73f367f5571ce1449fd288800c8419
bonono63 <bonoria63@gmail.com>
2022-05-21 00:21:14 -0400

    Added the Contraption Core GUI to the block entity

--- src/main/java/net/mrbonono63/scarlet/blocks/entities/ContraptionCoreBlockEntity.java
+++ src/main/java/net/mrbonono63/scarlet/blocks/entities/ContraptionCoreBlockEntity.java
@@ -1,22 +1,25 @@
 package net.mrbonono63.scarlet.blocks.entities;
 
-import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.network.PacketByteBuf;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.mrbonono63.scarlet.client.gui.ContraptionScreenDescription;
 
-public class ContraptionCoreBlockEntity extends BlockEntity implements ExtendedScreenHandlerFactory {
+public class ContraptionCoreBlockEntity extends BlockEntity implements NamedScreenHandlerFactory{
+    private int weight = 10;
+    private boolean isAssembled;
 
     public ScreenHandler createMenu(int syncId, PlayerInventory playerInventory, PlayerEntity player) {
-
-        return new BoxScreenHandler(syncId, playerInventory, this);
+        ScreenHandlerContext context = ScreenHandlerContext.create(world, pos);
+        return new ContraptionScreenDescription(syncId, playerInventory, context);
     }
 
     public ContraptionCoreBlockEntity(BlockPos pos, BlockState state) {
@@ -30,8 +33,26 @@ public class ContraptionCoreBlockEntity extends BlockEntity implements ExtendedS
         return Text.of("AMONGUS HAHA FUNNY");
     }
 
-    @Override
-    public void writeScreenOpeningData(ServerPlayerEntity player, PacketByteBuf buf) {
+    public int getWeight()
+    {
+        return this.weight;
+    }
 
+    //Tells the BlockEntity to assemble the contraption
+    public void Assemble()
+    {
+        if (!isAssembled) {
+            isAssembled = true;
+        }
+    }
+
+    @Override
+    protected void writeNbt(NbtCompound nbt) {
+        super.writeNbt(nbt);
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        super.readNbt(nbt);
     }
 }
