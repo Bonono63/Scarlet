9b77aafcbf60681102cec53d6ee4f9e9933d6802
bonono63 <bonoria63@gmail.com>
2022-05-21 00:14:16 -0400

    Added block cloning to Contraption Dimension (still needs major work for block entity cloning and proper allocation for blocks and contraption management)

--- src/main/java/net/mrbonono63/scarlet/server/ContraptionDimensionHandler.java
+++ src/main/java/net/mrbonono63/scarlet/server/ContraptionDimensionHandler.java
@@ -4,15 +4,11 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockBox;
-import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Vec3i;
-import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.World;
-import net.minecraft.world.chunk.ChunkManager;
 import net.mrbonono63.scarlet.Main;
+import net.mrbonono63.scarlet.util.CloneUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,16 +16,19 @@ import java.util.List;
 public class ContraptionDimensionHandler {
 
     //List of the contraptions the server is keeping track of
-    public List<BlockBox> originList = new ArrayList<>();
-    public List<BlockBox> denstinationList = new ArrayList<>();
+    public List<Contraption> activeList = new ArrayList<>();
+    public List<Contraption> addList = new ArrayList<>();
+    public List<Contraption> removeList = new ArrayList<>();
 
     int previousListSize = 0;
     int ListSize = 0;
 
-    //maximum entity length
+    //maximum contraption length in blocks
     public static final int MAXIMUM_BLOCK_LENGTH = 128;
-    //length in chunks of the maximum block length
-    public static final int CHUNK_SIZE = MAXIMUM_BLOCK_LENGTH / 16;
+    //maximum contraption length in chunks
+    public static final int MAXIMUM_CONTRAPTION_CHUNK_LENGTH = 8;
+    //space in between allocated chunk spaces
+    public static final int CHUNK_OFFSET = 1;
 
     //constructor
     public ContraptionDimensionHandler() {
@@ -37,53 +36,103 @@ public class ContraptionDimensionHandler {
     }
 
     //List management
-    public void addContraption(BlockBox box)
+    public int addContraption(Contraption contraption)
     {
-        originList.add(box);
+        addList.add(new Contraption(contraption.getOriginBlockBox(), contraption.getServerWorld()));
+        return addList.size();
     }
 
     public  void removeContraption(int i)
     {
-        originList.remove(i);
+        removeList.add(activeList.get(i));
+    }
+
+    public int offsetChunk(int a)
+    {
+        /*  Current chunk position (inside the loop)
+            + 1 (add the buffer we have for the current contraption)
+             + 1*number of active contraptions (adjust for number of current offsets already present)
+             + the max contraption length (8 chunks)*the active contraption list
+
+             The goal of this function is to force load the chunks the contraption will use
+             I don't know how to manage the chunks in a better way perhaps a list of chunk coordinates or a custom data
+             type could organize and manage the chunk loading better, however that is for future improvement
+             Also only force loading chunks with actual content (blocks or block entities) will improve performance.
+         */
+        return a+CHUNK_OFFSET+CHUNK_OFFSET*activeList.size()+MAXIMUM_CONTRAPTION_CHUNK_LENGTH*activeList.size();
+    }
+
+    //Similar to the chunk offset however instead it calculates the block coordinates instead of the chunk coordinates.
+    public int offsetPosition(int a)
+    {
+
+        /*  Current chunk position (inside the loop)
+            + 1 (add the buffer we have for the current contraption) * 16
+             + 1*number of active contraptions (adjust for number of current offsets already present)
+             + the max contraption length (8 chunks)*the active contraption list
+        * */
+
+        return a+CHUNK_OFFSET*16+CHUNK_OFFSET*16*activeList.size()+MAXIMUM_CONTRAPTION_CHUNK_LENGTH*16*activeList.size();
     }
 
     //Run every tick to add and remove the contraption block boxes passed through and manage that
     public void tick(ServerWorld serverWorld)
     {
         this.previousListSize = this.ListSize;
-        this.ListSize = originList.size();
+        this.ListSize = activeList.size();
 
         if (!serverWorld.isClient()) {
-            /*for (int i = 0; i < originList.size(); i++) {
-
-            }
-             */
-
-            //serverWorld.getChunkManager().setChunkForced(new ChunkPos(0,0), true);
-
             MinecraftServer server = serverWorld.getServer();
 
-            serverWorld.setChunkForced(0,0,true);
-
+            //serverWorld.setChunkForced(0,0,true);
 
             if (server.getWorldRegistryKeys().contains(RegistryKey.of(Registry.WORLD_KEY, new Identifier("scarlet", "contraption"))))
             {
                 ServerWorld contraptionDimension =  server.getWorld(RegistryKey.of(Registry.WORLD_KEY, new Identifier("scarlet", "contraption")));
-                contraptionDimension.setChunkForced(0,0, true);
-                Main.LOGGER.info(contraptionDimension.getForcedChunks().toString());
+
+                if (!addList.isEmpty())
+                {
+                    for (int i = 0 ; i < addList.size() ;i++) {
+                        //Get the first Box in the list
+                        BlockBox origin = addList.get(0).getOriginBlockBox();
+
+                        //allocate forced chunks for the contraption
+                        for (int x = 0; x < MAXIMUM_CONTRAPTION_CHUNK_LENGTH; x++) {
+                            for (int y = 0; y < 8; y++) {
+                                contraptionDimension.setChunkForced(offsetChunk(x), offsetChunk(y), true);
+                            }
+                        }
+
+                        BlockBox destination = makeDestinationBlockBox();
+
+                        CloneUtil.cloneToContraptionDimension(serverWorld, origin, destination);
+
+                        //add the box to the active list and remove the box from the add list
+                        activeList.add(addList.get(0));
+                        addList.remove(0);
+                        Main.LOGGER.info("new contraption added to the contraption dimension");
+                    }
+                }
+
+                //Main.LOGGER.info(contraptionDimension.getForcedChunks().toString());
             }
         }
     }
 
-    //Adding of the blocks to the worlds theme selves
-    public static void update(ServerWorld serverWorld, BlockBox origin, BlockBox destination)
+    //decides the space the contraption will be placed inside of
+    public BlockBox makeDestinationBlockBox()
     {
+        // number of blocks allocated to each contraption + the buffer chunks length in blocks
+        int x = activeList.size()*MAXIMUM_BLOCK_LENGTH+16*activeList.size();
+        int y = activeList.size()*MAXIMUM_BLOCK_LENGTH+16*activeList.size();
+        int z = 0;
 
-    }
+        // the origin pos + the size of the allocated space + maximum height
+        int i = x+MAXIMUM_BLOCK_LENGTH;
+        int j = y+MAXIMUM_BLOCK_LENGTH;
+        int k = 128;
 
-    public static BlockBox makeDestinationBlockBox(BlockBox origin)
-    {
-        return BlockBox.create(new Vec3i(origin.getMinX(), origin.getMinY(), origin.getMinZ()), new Vec3i(origin.getMaxX(), origin.getMaxY(), origin.getMaxZ()));
+        return BlockBox.create(new Vec3i(x, y, z), new Vec3i(i, j, k));
     }
 
     //assuming that the new contraption is added on the end of the contraptionList this will get the contraption's
@@ -94,8 +143,8 @@ public class ContraptionDimensionHandler {
     }
 
     //returns the contraption at the desired index
-    public BlockBox getContraption(int listIndex)
+    public Contraption getContraption(int listIndex)
     {
-        return this.originList.get(listIndex);
+        return this.activeList.get(listIndex);
     }
 }
